3ccc57ab9304250b2607cb1cf276fb9685649fba
so jeong <ssoo9313@daum.net>
2026-09-15 22:28:20 +0900

    Update server port for Render

--- server.js
+++ server.js
@@ -96,8 +96,10 @@ if (url.pathname === "/api/address-search") {
   });
 });
 
-server.listen(3000, () => {
-  console.log("서버 실행 중: http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+ console.log(`서버 실행 중: http://localhost:${PORT}`);
 });
 console.log(
   "VWorld API 키:",
